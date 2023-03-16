690ed48ca4d4c71e37b0a10ab5c3f23d00375115
lmxx1025 <72892938+lmxx1025@users.noreply.github.com>
2023-03-16 16:26:28 +0800

    Initial ddd 工程初始化

---
+++ chatbot-api-interfaces/src/main/java/cn/xiaocheng/chatbot/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.xiaocheng.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小程
+ * @description 启动入口
+ * @github  https://github.com/lmxx1025
+ * @Copuright xxx
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
